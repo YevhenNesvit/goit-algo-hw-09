e6e492a672318beb093da66a0e32b93201581ca7
Yevhen Nesvit <yevhen.nesvit@gmail.com>
2024-01-31 18:02:48 +0200

    add greedy and dynamic funcs

---
+++ coins_count.py
@@ -0,0 +1,28 @@
+def find_coins_greedy(amount):
+    coins = [50, 20, 10, 5, 2, 1]  # доступні номінали монет
+    coins_count = {}
+
+    for coin in coins:
+        if amount >= coin:
+            coins_count[coin] = amount // coin
+            amount %= coin
+
+    return coins_count
+
+
+def find_min_coins(amount):
+    coins = [1, 2, 5, 10, 20, 50]  # доступні номінали монет
+    coins_count = {}
+
+    for coin in coins[::-1]:
+        while amount >= coin:
+            if coin not in coins_count:
+                coins_count[coin] = 0
+            coins_count[coin] += 1
+            amount -= coin
+
+    return coins_count
+
+
+print(find_coins_greedy(113))
+print(find_min_coins(113))
